c544da5b05a319c0bcff5613980758fa664efd20
Karolina Pikus <karolinapikus@MacBook-Pro-Karolina.local>
2019-12-21 11:12:38 +0100

    Login layout added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,12 +3,14 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.scss';
 
 import Home from "../Home/Home"
+import Login from "../Login/Login"
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home}/>
+        <Route path="/zaloguj" component={Login}/>
       </Switch>
     </Router>
 
---
+++ src/components/Login/Login.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Link, Switch, Route} from 'react-router-dom';
+import './Login.scss';
+
+
+import Nav from "../Nav/Nav"
+class Login extends Component {
+    render(){
+        return (
+            <>
+            <Nav />
+            <div className="login__form">
+<div className="login__header">
+    <h2>Zaloguj się</h2>
+    <div className="header-divider"></div>
+</div>
+    <form>
+        <div class="form-fields">
+        <label for="email">E-mail</label>
+        <input type="mail" name="email" />
+        <label for="password">Hasło</label>
+        <input type="password" name="password" />
+        </div>
+    </form>
+    <div class="login__links">
+    <ul className="login-nav">
+    <li><Link to="/zaloz-konto">Załóz konto</Link></li>
+                        <li><Link to="/zaloguj">Zaloguj się</Link></li>
+                        
+                    </ul>
+    </div>
+
+            </div>
+            </>
+        )
+    }
+}
+
+export default Login;
